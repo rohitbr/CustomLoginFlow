cfa92f5b9b09d3d9960fa92c4370747c903dded5
Your Name <rbhat@getluma.com>
2020-04-13 15:50:10 -0400

    Sign up view

--- CustomWelcomeVC/CustomWelcomeVC/SignupView.swift
+++ CustomWelcomeVC/CustomWelcomeVC/SignupView.swift
@@ -20,19 +20,52 @@ struct SignupView: View {
 
             Spacer()
 
-            TextField("Username", text: $viewModel.username)
-                .textFieldStyle(CustomTextFieldStyle())
-            TextField("Password", text: $viewModel.password)
-                .textFieldStyle(CustomTextFieldStyle())
-            TextField("Confirm Password", text: $viewModel.confirmPassword)
-                .textFieldStyle(CustomTextFieldStyle())
+            Group {
+                TextField("Username", text: $viewModel.username)
+                    .textFieldStyle(CustomTextFieldStyle())
+                HStack() {
+                    Text(self.viewModel.usernameDesc.isEmpty ? "Email pattern good" : self.viewModel.usernameDesc)
+                        .lineLimit(nil)
+                        .font(.subheadline)
+                        .foregroundColor(self.viewModel.usernameDesc.isEmpty ? Color.green : Color.red)
+                        .padding(.leading)
+
+                    Spacer()
+                }
+
+                TextField("Password", text: $viewModel.password)
+                    .textFieldStyle(CustomTextFieldStyle())
+
+                HStack() {
+                    Text(self.viewModel.originalPasswordDesc.isEmpty ? "Passwords pattern good" : self.viewModel.originalPasswordDesc)
+                        .lineLimit(nil)
+                        .font(.subheadline)
+                        .foregroundColor(self.viewModel.originalPasswordDesc.isEmpty ? Color.green : Color.red)
+                        .padding(.leading)
+                    Spacer()
+                }
+
+                TextField("Confirm Password", text: $viewModel.confirmPassword)
+                    .textFieldStyle(CustomTextFieldStyle())
+
+                HStack() {
+                    Text(self.viewModel.passwordsValid ? "Passwords match" : "Passwords do not match")
+                        .lineLimit(nil)
+                        .font(.subheadline)
+                        .foregroundColor(self.viewModel.passwordsValid ? Color.green : Color.red)
+                        .padding(.leading)                    
+                    Spacer()
+                }
+            }
 
             Spacer()
-
+            
             NavigationLink(destination: HomeView(), isActive: $viewModel.goToHome) {
                 RedRoundedButton("Sign up") {
                     self.viewModel.buttonActionSignup()
                 }
+                .disabled(self.viewModel.allFieldsValid ? false : true)
+                .opacity(self.viewModel.allFieldsValid ?  1 : 0.6 )
             }
             Spacer()
                 .frame(height:20)
--- CustomWelcomeVC/CustomWelcomeVC/SignupViewModel.swift
+++ CustomWelcomeVC/CustomWelcomeVC/SignupViewModel.swift
@@ -18,6 +18,84 @@ class SignupViewModel: ObservableObject {
     @Published public var showModal = false
     @Published public var gotoHomeView = false
     @Published public var description = ""
+    @Published public var usernameDesc = ""
+    @Published public var passwordsValid = false
+    @Published public var originalPasswordDesc = ""
+    @Published public var allFieldsValid = false
+
+    private var emailDesc : AnyPublisher<String, Never> {
+        return $username
+            .removeDuplicates()
+            .flatMap { username in
+                return Future { promise in
+                    let result = InputValidator.validate(email: username)
+                    promise(.success(result.errorReason ?? ""))
+                }
+        }
+        .eraseToAnyPublisher()
+    }
+
+    private var passDesc : AnyPublisher<String, Never> {
+           return $password
+               .removeDuplicates()
+               .flatMap { password in
+                   return Future { promise in
+                       let result = InputValidator.validate(password: password )
+                       promise(.success(result.errorReason ?? ""))
+                   }
+           }
+           .eraseToAnyPublisher()
+       }
+
+    private var validatePasswords : AnyPublisher <Bool, Never> {
+        return Publishers.CombineLatest($password, $confirmPassword)
+            .map {  password, passwordConfirm in
+                let resultPassword = InputValidator.validate(password: password)
+                let resultPasswordConfirm = InputValidator.validate(password: passwordConfirm)
+
+                guard resultPassword.isValid,
+                    resultPasswordConfirm.isValid,
+                    password == passwordConfirm else {
+                    return false
+            }
+            return true
+        }
+        .eraseToAnyPublisher()
+    }
+
+    private var validateAllfields : AnyPublisher <Bool, Never> {
+        return Publishers.CombineLatest(emailDesc, validatePasswords)
+            .map {  emailDesc, validatePasswords in
+                guard emailDesc.isEmpty && validatePasswords
+                    else {
+                    return false
+            }
+            return true
+        }
+        .eraseToAnyPublisher()
+    }
+
+
+    init() {
+        var subscriptions = Set<AnyCancellable>()
+        
+        emailDesc
+            .receive(on: DispatchQueue.main)
+            .assign(to : \.usernameDesc, on : self)
+            .store(in : &subscriptions)
+        passDesc
+            .receive(on: DispatchQueue.main)
+            .assign(to : \.originalPasswordDesc, on : self)
+            .store(in : &subscriptions)
+        validatePasswords
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.passwordsValid, on: self)
+            .store(in: &subscriptions)
+        validateAllfields
+                   .receive(on: DispatchQueue.main)
+                   .assign(to: \.allFieldsValid, on: self)
+                   .store(in: &subscriptions)
+    }
 
     let kSignupSuccessful = "Sign up successful"
 
